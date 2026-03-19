31bc5e1caed9379856c0c75024050392966888f1
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-03-19 17:12:33 +0530

    Time: 4 ms (56.28%), Space: 18.1 MB (83.68%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>>adj(n);
         vector<int>indegree(n,0);
+        vector<vector<int>>adj(n);
 
-        for(auto &p:prerequisites){
-            int x=p[0];
-            int y=p[1];
-            adj[y].push_back(x);
+        for(int i=0;i<prerequisites.size();i++){
+            int x=prerequisites[i][0];
+            int y=prerequisites[i][1];
+
             indegree[x]++;
+            adj[y].push_back(x);
         }
+
         queue<int>q;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+            if(indegree[i]==0)q.push(i);
         }
-
         vector<int>ans;
 
         while(!q.empty()){
-            int c=q.front();
+            int node=q.front();
             q.pop();
-            ans.push_back(c);
+            ans.push_back(node);
 
-            for(int i=0;i<adj[c].size();i++){
-                indegree[adj[c][i]]--;
-                if(indegree[adj[c][i]]==0){
-                    q.push(adj[c][i]);
+            for(auto x:adj[node]){
+                int new_node=x;
+                indegree[new_node]--;
+                if(indegree[new_node]==0){
+                    q.push(new_node);
                 }
             }
 
         }
-        if(ans.size()!=n)return {};
-        else return ans;
+
+       if(ans.size()==n)return ans;
+       return {};
     }
 };
