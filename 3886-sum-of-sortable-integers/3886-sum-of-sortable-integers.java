c6c151d9f6c671ec00fe97ce2fb4a77014487b55
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-04-09 20:06:52 +0530

    Time: 69 ms (48.58%), Space: 76.3 MB (39.69%) - LeetHub

--- 3886-sum-of-sortable-integers/3886-sum-of-sortable-integers.java
+++ 3886-sum-of-sortable-integers/3886-sum-of-sortable-integers.java
@@ -14,25 +14,6 @@ class Solution {
     return (ct<=1);
     }
 
-    public boolean areCorrectNumsPresent(int i , int j, int []nums,int []sorted){
-        int []arr= new int[j-i+1];
-        int ind=0;
-        for(int k=i;k<=j;k++){arr[ind]=nums[k];ind++;}
-        Arrays.sort(arr);
-        ind=0;
-        int ind2=i;
-        while(ind<arr.length){
-            if(arr[ind]!=sorted[ind2]){
-                return false;
-            }
-            ind++;
-            ind2++;
-        }
-
-        return true;
-
-    }
-
     public int sortableIntegers(int[] nums) {
         int n = nums.length;
 
@@ -43,13 +24,22 @@ class Solution {
 
         for (int k = 1; k <= n; k++) {
             if (n % k != 0) continue;
+            int mn=0;
             boolean ok = true;
             for (int i = 0; i < n; i += k) {
                 int j = i+k-1;
-                if(!areCorrectNumsPresent(i,j,nums,sorted) ||!canRotateToAsc(i,j,nums)){
+                int subarray_mn=Integer.MAX_VALUE;
+                int subarray_mx=Integer.MIN_VALUE;
+                for(int l=i;l<=(i+k-1);l++){
+                    subarray_mn=Math.min(subarray_mn,nums[l]);
+                    subarray_mx=Math.max(subarray_mx,nums[l]);
+                }
+
+                if(mn>subarray_mn || !canRotateToAsc(i,j,nums)){
                     ok=false;
                     break;
                 }
+                mn=subarray_mx;
             }
 
             if (ok) {
