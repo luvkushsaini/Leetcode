bc61365e11d3a2e7413c4a564be51c241a63e29f
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-04-24 00:54:39 +0530

    Time: 14 ms (30.41%), Space: 48.2 MB (15.29%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -1,23 +1,18 @@
 class Solution {
-    int n;
-    int [][]dp;
-    int solve(int i,int ct,int[] satisfaction){
-        if(i==n)return 0;
-
-        if(dp[i][ct]!=-1)return dp[i][ct];
-
-        int ans=satisfaction[i]*(ct+1)+solve(i+1,ct+1,satisfaction);
-
-        ans=Math.max(ans,solve(i+1,ct,satisfaction));
-
-        return dp[i][ct]=ans;
-    }
     public int maxSatisfaction(int[] satisfaction) {
-        n=satisfaction.length;
-        dp=new int[n][n+1];
-        for(int i=0;i<n;i++)Arrays.fill(dp[i],-1);
+        int n=satisfaction.length;
+        int [][]dp=new int[n+1][n+1];
         Arrays.sort(satisfaction);
 
-        return solve(0,0,satisfaction);
+        for(int ct=0;ct<=n;ct++)dp[n][ct]=0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int ct=i;ct>=0;ct--){
+                dp[i][ct]=(ct+1)*satisfaction[i]+dp[i+1][ct+1];
+                dp[i][ct]=Math.max(dp[i][ct],dp[i+1][ct]);
+            }
+        }
+
+        return dp[0][0];
     }
 }
