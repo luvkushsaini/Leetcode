dbd202e594b96152487cf98d0b594354a86b25e0
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-07-02 11:06:40 +0530

    Time: 91 ms (19.72%), Space: 55.5 MB (17.26%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-     vector<int>r={-1,1,0,0};
-     vector<int>c={0,0,-1,1};
-     int n,m;
+    // previous submitted solution was based on the bfs using priority queue as we can travel in all four direction 
+    int n, m;
+    vector<int> dr = {-1, 1, 0, 0};
+    vector<int> dc = {0, 0, -1, 1};
+    vector<vector<vector<int>>> dp;
 
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        n=grid.size();
-        m=grid[0].size();
+    bool solve(int i, int j, int health, vector<vector<int>>& grid) {
+        if (health <= 0)
+            return false;
+        if (i == n - 1 && j == m - 1)
+            return true;
+        if (dp[i][j][health] != -1)
+            return dp[i][j][health];
 
-        priority_queue<tuple<int,int,int>>pq;
-        pq.push({health-grid[0][0],0,0});
+        dp[i][j][health] = 0;
 
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        for (int k = 0; k < 4; k++) {
+            int x = i + dr[k];
+            int y = j + dc[k];
 
-
-        while(!pq.empty()){
-            auto [currHealth,i,j]=pq.top();
-            pq.pop();
-            if(vis[i][j]==1 || currHealth==0)continue;
-            if(i==n-1 && j==m-1)return true;
-            vis[i][j]=1;
-
-            for(int k=0;k<4;k++){
-                int x=i+r[k];
-                int y=j+c[k];
-                if(x>=0 && x<n && y>=0 && y<m && vis[x][y]==0){
-                    int updatedHealth=currHealth-grid[x][y];
-                    pq.push({updatedHealth,x,y});
-                }
+            if (x >= 0 && x < n && y >= 0 && y < m) {
+                int newHealth = health - grid[x][y];
+                if (solve(x, y, newHealth, grid))
+                    return dp[i][j][health] = 1;
             }
         }
 
-        return false;
+        return dp[i][j][health];
+    }
 
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        n = grid.size();
+        m = grid[0].size();
+
+        health -= grid[0][0];
+        if (health <= 0)
+            return false;
+
+        dp.assign(n, vector<vector<int>>(m, vector<int>(health + 1, -1)));
+        return solve(0, 0, health, grid);
     }
 };
