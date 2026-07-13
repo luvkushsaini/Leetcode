2e447f20979f3bd3becfc65c1d2b1b1763284534
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-07-13 12:06:12 +0530

    Time: 27 ms (94.28%), Space: 13.8 MB (89.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
+   //reduced to the 3d dp as i and k will always be same as we are always moving one row down for both the robots
     vector<int>row={1,1,1};
     vector<int>col={-1,0,1};
-    int dp[70][70][70][70];
+    int dp[70][70][70];
     int n,m;
-    int solve(int i,int j,int k,int l,vector<vector<int>>&grid){
-        if(dp[i][j][k][l]!=-1)return dp[i][j][k][l];
+    int solve(int i,int j,int l,vector<vector<int>>&grid){
+        int k=i;
+        if(dp[i][j][l]!=-1)return dp[i][j][l];
         int cherries=(i==k && j==l)?(grid[i][j]):(grid[i][j]+grid[k][l]);
         int path=0;
         for(int d1=0;d1<3;d1++){
@@ -16,11 +18,11 @@ public:
                 int l1=l+col[d2];
 
                 if(i1>=0 && i1<n && j1>=0 && j1<m && k1>=0 && k1<n && l1>=0 && l1<m){
-                     path=max(path,solve(i1,j1,k1,l1,grid));
+                     path=max(path,solve(i1,j1,l1,grid));
                 }
             }
         }
-        return dp[i][j][k][l]=(cherries+path); 
+        return dp[i][j][l]=(cherries+path); 
     }
 
     int cherryPickup(vector<vector<int>>& grid) {
@@ -28,7 +30,7 @@ public:
         m=grid[0].size();
         memset(dp,-1,sizeof(dp));
 
-        int ans=solve(0,0,0,m-1,grid);
+        int ans=solve(0,0,m-1,grid);
         return ans;
     }
 };
