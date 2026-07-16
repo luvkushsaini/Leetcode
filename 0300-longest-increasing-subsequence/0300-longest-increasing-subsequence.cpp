876b4b1ac97b4fcbde097fd5639d08178e6f243a
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-07-16 17:06:06 +0530

    Time: 51 ms (70.2%), Space: 14.5 MB (48.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -4,15 +4,17 @@ public:
         int n=nums.size();
         vector<int>dp(n,1);
         int ans=1;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[j]<nums[i])dp[i]=max(dp[i],1+dp[j]);
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                    ans=max(dp[i],ans);
+                }
             }
-            ans=max(dp[i],ans);
         }
         return ans;
 
+    
 
     }
 };
