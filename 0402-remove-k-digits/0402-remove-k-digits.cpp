5522f15cf188d0d5c9ef53ebd9778ccb9eb25c7f
Lovkush <146121400+luvkushsaini@users.noreply.github.com>
2026-07-16 11:58:48 +0530

    Time: 3 ms (62.2%), Space: 12 MB (17.78%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-  vector<int>nse(string &num){
-    int n=num.size();
-    vector<int>v(n);
-    stack<int>st;
-
-    for(int i=n-1;i>=0;i--){
-        while(!st.empty() && num[i]<=num[st.top()])st.pop();
-        if(!st.empty())v[i]=st.top();
-        if(st.empty())v[i]=n;
-        st.push(i);
-    }
-    return v;
-  }
-    string removeKdigits(string num, int k) {
-        int n=num.size();
-        vector<int>nseIndex=nse(num);
-        string ans="";
+    string removeKdigits(string s, int k) {
+        int n=s.size();
+        stack<int>st;
         int i=0;
-       while(i<n && k>0){
-            if((nseIndex[i]-i)>k ){
-                if(ans.size()==0  && num[i]=='0'){
-                    i++;
-                    continue;
-                }
-                ans.push_back(num[i]);
-                i++;
-            }
-            else if ((nseIndex[i]-i)<=k){
-                k-=(nseIndex[i]-i);
-                i=nseIndex[i];
-            }
-        }
-
+        char c=2+'0';
         while(i<n){
-            if(ans.size()==0  && num[i]=='0'){
-                    i++;
-                    continue;
-                }
-                ans.push_back(num[i]);
+            int num=s[i];
+            while(!st.empty() && st.top()>num && k>0){
+                st.pop();
+                k--;
+            }
+            if(st.empty() && num=='0'){
                 i++;
+                continue;
+            }
+            st.push(num);
+            i++;
         }
-        if(ans.size()==0)return "0";
-        return ans;
+        while(k>0 && !st.empty()){
+            st.pop();
+            k--;
+        }
 
+        string ans="";
+        while(!st.empty())ans.push_back(st.top()),st.pop();
+        if(ans.size()==0)return "0";
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
